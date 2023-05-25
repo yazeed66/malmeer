29c8cf5c296fde9909f8a49ac561698b01be6e45
Alialfadhli <134432687+Alialfadhli@users.noreply.github.com>
2023-05-25 16:31:05 +0300

    Delete Communication.py

--- Communication.py
+++
@@ -1,9 +0,0 @@
-import socket
-cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-msg = input("Send")
-
-cs.sendto(msg.encode('ascii'),('127.0.0.1',49999))
-data, address = cs.recvfrom(2048)
-print('Received: ', data.decode('ascii'))
-""hi""
-cs.close
