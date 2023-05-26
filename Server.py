91032fcd24ca264c990c470a932f5becc72c8fb7
Alialfadhli <134432687+Alialfadhli@users.noreply.github.com>
2023-05-26 21:17:22 +0300

    Add files via upload

--- Server.py
+++ Server.py
@@ -6,3 +6,12 @@ import pickle
 import threading
 import urllib.request
 from datetime import datetime
+buffer_size =1000
+PORT = 55550
+encoding_type="utf-8"
+SERVER_IP="127.0.0.3"
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.Bind((SERVER_IP , PORT))
+NumOfAcceptedClients=10
+JsonFile="GI.json"
+ClientLists = []
