439cc5740b218e90666603eb6738e777f238fde8
Alialfadhli <134432687+Alialfadhli@users.noreply.github.com>
2023-05-26 21:30:46 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -15,3 +15,22 @@ SERVER.Bind((SERVER_IP , PORT))
 NumOfAcceptedClients=10
 JsonFile="GI.json"
 ClientLists = []
+ShutDownServer = True
+accessKey = "a2824f8004d5a49d6c5118a51214a95e"
+LimitOfRetrievedRecords = 100
+
+def Get_Flights_Data():
+    try:
+        while True:
+            AirportCode=""
+            while AirportCode ==""
+            AirportCode=str(input("Enter the Airport Code"))
+            WebUrl = urllib.request.urlopen
+            (
+                "http://api.aviationstack.com/v1/flights?access_key=" + str(access_key) + "&limit=" + str(
+                    limit_of_retrieved_records) + "&arr_icao=" + str(Airportcode)
+                    data = webUrl.read()
+                    jsonData = json.loads(data)
+                    if
+            
+            )
