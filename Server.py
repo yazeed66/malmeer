bbb238cd3b41a7bd5580c20a8a5d0161fbe156fb
yazeed66 <134303741+yazeed66@users.noreply.github.com>
2023-05-27 09:10:26 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -81,5 +81,127 @@ def Get_All_Delayed_Flights():
         with open(JsonFile) as f:
             data = json.load(f)
             for x in data["data"]:
-            
-            )
+                 if str(x["departure"]["delay"]) != "None":
+                    Response.append(x["flight"]["iata"])
+                    Response.append(x["departure"]["airport"])
+                    Response.append(x["departure"]["estimated"])
+                    Response.append(x["arrival"]["estimated"])
+                    Response.append(x["arrival"]["gate"])
+                    if x["arrival"]["terminal"] == None or x["arrival"]["terminal"] == "" or str(
+                            x["arrival"]["terminal"]) == "null":
+                        Response.append("None")
+                    else:
+                        Response.append(x["arrival"]["terminal"])
+                    Search_Flag = True
+        if Search_Flag == True:
+            return pickle.dumps(Response)
+        else:
+            return pickle.dumps("There is no Delayed Flights !")
+    except Exception as e:
+        print("[" + str(datetime.now()) + "]" + " - " + "Error While Getting Delayed Flights!")
+        return pickle.dumps("Excepetion Result : " + str(e) + "[" + str(
+            datetime.now()) + "]" + " - " + "Error While Getting Delayed Flights!")
+def Get_Flights_From_Specific_City(City_Name):
+    try:
+        Search_Flag = False
+        Response = []
+        with open(JsonFile) as f:
+            data = json.load(f)
+            for x in data["data"]:
+                City = str(x["departure"]["timezone"]).split("/")
+                if str(City[-1]).lower() == str(City_Name).lower():
+                    Response.append(x["flight"]["iata"])
+                    Response.append(x["departure"]["airport"])
+                    Response.append(x["departure"]["timezone"])
+                    Response.append(x["arrival"]["estimated"])
+                    Response.append(str(x["arrival"]["gate"]))
+                    if x["arrival"]["terminal"] == "":
+                        Response.append("NULL")
+                    else:
+                        Response.append(str(x["arrival"]["terminal"]))
+                    Search_Flag = True
+        if Search_Flag == True:
+            return pickle.dumps(Response)
+        else:
+            return pickle.dumps("There is no Flights Comes from: (" + str(City_Name) + ")!")
+    except Exception as e:
+        print("[" + str(datetime.now()) + "]" + " - " + "Error While Getting Flights From " + City_Name + "!")
+        return pickle.dumps("Excepetion Result : " + str(e) + "[" + str(
+            datetime.now()) + "]" + " - " + "Error While Getting Flights From " + City_Name + "!")
+def Get_Details_of_Particular_Flight(Flight_IATA):
+    try:
+        Search_Flag = False
+        Response = []
+        with open(JsonFile) as f:
+            data = json.load(f)
+            for x in data["data"]:
+                IATA = str(x["flight"]["iata"]).lower()
+                if IATA.lower() == str(Flight_IATA).lower():
+                    Response.append(x["flight"]["iata"])
+                    Response.append(x["flight_date"])
+                    Response.append(x["departure"]["airport"])
+                    Response.append(str(x["departure"]["terminal"]))
+                    Response.append(str(x["departure"]["gate"]))
+                    Response.append(x["arrival"]["airport"])
+                    Response.append(str(x["arrival"]["gate"]))
+                    Response.append(str(x["arrival"]["terminal"]))
+                    Search_Flag = True
+        if Search_Flag == True:
+            return pickle.dumps(Response)
+        else:
+            return pickle.dumps("IATA Flight: (" + str(Flight_IATA) + ") Not Found!")
+    except Exception as e:
+        print("Excepetion Result : " + str(e) + "[" + str(
+            datetime.now()) + "]" + " - " + "Error While Getting Flight Number: " + Flight_IATA + "!")
+        return pickle.dumps("Excepetion Result : " + str(e) + "[" + str(
+            datetime.now()) + "]" + " - " + "Error While Getting Flight Number: " + Flight_IATA + "!")
+def define_connection(connection, name):
+    ClientLists.append(name)
+    print("[" + str(datetime.now()) + "]" + " - ", name, " IS CONNECTED NOW ")
+    print("[" + str(datetime.now()) + "]" + " - ACTIVE CLIENTS: " + str(ClientLists))
+    connection.sendall(pickle.dumps(ClientLists))
+    while True:
+        received_option = connection.recv(buffer_size).decode(encoding_type)
+        if str(received_option) == "1":
+            print("[" + str(datetime.now()) + "]" + " - " + name + " SELECT OPTION : " + str(received_option))
+            connection.sendall((Get_All_Arrived_Flights()))
+        elif str(received_option) == "2":
+            print("[" + str(datetime.now()) + "]" + " - " + name + " SELECT OPTION : " + str(received_option))
+            connection.sendall(Get_All_Delayed_Flights())
+        elif str(received_option) == "3":
+            print("[" + str(datetime.now()) + "]" + " - " + name + " SELECT OPTION : " + str(received_option))
+            Recived_City = connection.recv(buffer_size).decode(encoding_type)
+            print("[" + str(datetime.now()) + "]" + " - " + name + " ASK ABOUT CITY : " + str(Recived_City))
+            connection.sendall(Get_Flights_From_Specific_City(Recived_City))
+        elif str(received_option) == "4":
+            print("[" + str(datetime.now()) + "]" + " - " + name + " SELECT OPTION : " + str(received_option))
+            Recived_IATA_Flight = connection.recv(buffer_size).decode(encoding_type)
+            print(
+                "[" + str(datetime.now()) + "]" + " - " + name + " ASK ABOUT IATA Flight : " + str(Recived_IATA_Flight))
+            connection.sendall(Get_Details_of_Particular_Flight(Recived_IATA_Flight))
+        elif str(received_option) == "5":
+            print("[" + str(datetime.now()) + "]" + " - " + name + " SELECT OPTION : " + str(received_option))
+            connection.sendall(pickle.dumps(ClientLists))
+        elif str(received_option) == "6":
+            print("[" + str(datetime.now()) + "]" + " - " + name + " SELECT OPTION : " + str(received_option))
+            ClientLists.remove(name)
+            connection.sendall(pickle.dumps(ClientLists))
+            print("[" + str(datetime.now()) + "]" + " - " + name + " IS DISCONNECTED")
+            print("[" + str(datetime.now()) + "]" + " - ACTIVE CLIENTS: " + str(ClientLists))
+            connection.close()
+            if (int(threading.activeCount()) - 1) == 1 and ShutDownServer == True:
+                SERVER_SHUTDOWN()
+            break
+def start_new_connection():
+    global SERVER
+    SERVER.listen(NumOfAcceptedClients)
+    print("[" + str(datetime.now()) + "]" + " <<<<<<< server is starting >>>>>")
+    while True:
+        connection, address = SERVER.accept()
+        Client_Name = connection.recv(buffer_size).decode(encoding_type)
+        if str(Client_Name) == "" or Client_Name == None or str(Client_Name) == "None":
+            continue
+        thread = threading.Thread(target=define_connection, args=(connection, Client_Name))
+        thread.start()
+Get_Flights_Data()
+start_new_connection()
