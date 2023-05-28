6e8d4469a812244160cb6085383067be1a5b271a
yazeed66 <134303741+yazeed66@users.noreply.github.com>
2023-05-28 16:52:10 +0300

    Add files via upload

---
+++ API.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+arr_icao = input("Enter ICAO \n>>")
+
+params = {"access_key": 'a2824f8004d5a49d6c5118a51214a95e',"arr_icao": arr_icao,"limit": 100}
+print("collecting the data from the API ")
+api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
+
+with open("GI.json", "w") as file:
+    json.dump(api_result.json(), file, indent=4)
+
+print("The Data stored in the file [GI.json]")
