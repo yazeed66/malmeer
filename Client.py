a5cfa6c7b8581b1ec8961ba5b1cfaed998196bdd
Alialfadhli <134432687+Alialfadhli@users.noreply.github.com>
2023-05-28 10:15:14 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -72,3 +72,119 @@ def serve_client():
                 j = j + 1
                 i = i + 5
             print(Response)
+        elif Selected_Option == 2:
+            Sending_Function("2")
+            Receviced_Data = Reciving_Function()
+            Response = ""
+            Response += (
+                "{:<6} | {:<17} | {:<25} | {:<8} | {:<8} | {:<8} \n".format('IATA'.center(6), 'Departure Airport'.center(25),'Departure Estimated'.center(25),
+                                                                    'ArivalEstimated'.center(25),
+                                                                    'Terminal'.center(8), 'Gate'.center(8)))
+            Response += (
+                "{:<6} | {:<17} | {:<25} | {:<8} | {:<8} | {:<8} \n".format('------'.center(6), '-----------------'.center(25),'-----------------'.center(25),
+                                                                    '-----------------'.center(25),
+                                                                    '--------'.center(8), '--------'.center(8)))
+
+            i = 0
+            j = 0
+            j = j + 1
+            while j <=  (len(Receviced_Data) / 6):
+                Response += ("{:<6} | {:<17} | {:<25} | {:<8} | {:<8}| {:<8} \n".format(str(Receviced_Data[i]).center(6),
+                                                                                 str(Receviced_Data[i + 1]).center(25),
+                                                                                 str(Receviced_Data[i + 2]).center(25),
+                                                                                 str(Receviced_Data[i + 3]).center(25),
+                                                                                 str(Receviced_Data[i + 4]).center(8),
+                                                                                 str(Receviced_Data[i + 5]).center(8),
+                                                                                 ))
+
+                j = j + 1
+                i = i + 6
+            print(Response)
+
+        elif Selected_Option == 3:
+            City_Name = ""
+            while City_Name == "":
+                City_Name = str(input("Please provide the name of the city you are Looking for:"))
+            Sending_Function("3")
+            Sending_Function(City_Name)
+
+            Receviced_Data = Reciving_Function()
+            Response = ""
+            Response += (
+                "{:<6} | {:<25} | {:<25} |{:<25} | {:<8} | {:<8} \n".format('IATA'.center(6), 'DepartureAirport'.center(25),
+                                                                    'Departuretimezone'.center(25),
+                                                                    'ArivalEstimated'.center(25),
+                                                                    'Arrivalgate'.center(8), 'Aterminal'.center(8)))
+            Response += (
+                "{:<6} | {:<25} | {:<25} |{:<25}  | {:<8} | {:<8} \n".format('------'.center(6), '-----------------'.center(25),
+                                                                    '-----------------'.center(25),
+                                                                    '-----------------'.center(25),
+                                                                    '--------'.center(8), '--------'.center(8)))
+
+            i = 0
+            j = 0
+            j = j + 1
+            while j <= (len(Receviced_Data) / 6):
+                Response += ("{:<6} | {:<25} | {:<25} |{:<25} | {:<8} | {:<8} \n".format(str(Receviced_Data[i]).center(6),
+                                                                                 str(Receviced_Data[i + 1]).center(25),
+                                                                                 str(Receviced_Data[i + 2]).center(25),
+                                                                                 str(Receviced_Data[i + 3]).center(25),
+                                                                                 str(Receviced_Data[i + 4]).center(8),
+                                                                                 str(Receviced_Data[i + 5]).center(8),
+                                                                                 ))
+
+                j = j + 1
+                i = i + 6
+            print(Response)
+
+        elif Selected_Option == 4:
+            Flight_Number = ""
+            while Flight_Number == "":
+                Flight_Number = str(input("Please provide the name of the Flight you are Looking for:"))
+            Sending_Function("4")
+            Sending_Function(Flight_Number)
+
+            Receviced_Data = Reciving_Function()
+            Response = ""
+            Response += (
+                "{:<6} | {:<10} | {:<25} |{:<10} | {:<8} | {:<25} |{:<10} | {:<8}\n".format('IATA'.center(6),'date'.center(10),'DepartureAirport'.center(25),
+                                                                    'DTerminal'.center(10),'DGate'.center(8),
+                                                                    'ArrivalAirport'.center(25),'ATerminal'.center(10),
+                                                                    'AGate'.center(8)))
+            Response += (
+                "{:<6} | {:<10} | {:<25} |{:<10} | {:<8} | {:<25} |{:<10} | {:<8}\n".format('------'.center(6),'----------'.center(10), '----------------'.center(25),
+                                                                    '-------'.center(10),'-------'.center(8),
+                                                                    '-----------------'.center(25),
+                                                                    '--------'.center(10), '--------'.center(8)))
+
+            i = 0
+            j = 0
+            j = j + 1
+            while j <= (len(Receviced_Data) / 8):
+                Response += ("{:<6} | {:<10} | {:<25} |{:<10} | {:<8} | {:<25} |{:<10} | {:<8}\n".format(str(Receviced_Data[i]).center(6),
+                                                                                 str(Receviced_Data[i+1]).center(10),
+                                                                                 str(Receviced_Data[i + 2]).center(25),
+                                                                                 str(Receviced_Data[i + 3]).center(10),
+                                                                                 str(Receviced_Data[i + 4]).center(8),
+                                                                                 str(Receviced_Data[i + 5]).center(25),
+                                                                                 str(Receviced_Data[i + 6]).center(10),
+                                                                                 str(Receviced_Data[i + 7]).center(8),
+                                                                                 ))
+
+                j = j + 1
+                i = i + 8
+            print(Response)
+        elif Selected_Option == 5:
+            Sending_Function("5")
+            print(f"\n  ## LIST OF USERS CURRENTLY CONNECTED TO THE SERVER. ## \n {str(Reciving_Function())}")
+        elif Selected_Option == 6:
+            Sending_Function("6")
+            print(f"\n  ## LIST OF USERS CURRENTLY CONNECTED TO THE SERVER.  ## \n {str(Reciving_Function())}")
+
+            print("BYE BYE SEE YOU SOON <3")
+            Client.close()
+            exit(0)
+        else:
+            serve_client()
+ask_for_client_name()
+serve_client()   
